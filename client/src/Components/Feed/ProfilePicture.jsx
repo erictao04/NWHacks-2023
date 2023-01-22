47b3f4e0dba660bba07a19227fbc6e00b7a29b6b
Eric Tao <erictao04@gmail.com>
2023-01-22 11:04:34 -0800

    Sign up page

--- client/src/Components/Feed/ProfilePicture.jsx
+++ client/src/Components/Feed/ProfilePicture.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function ProfilePicture({ url }) {
-  return <img style={{ height: '40px', borderRadius: '50%', cursor: 'pointer' }} src={url} />;
+  if (url) {
+    return <img style={{ height: '35px', borderRadius: '50%', cursor: 'pointer' }} src={url} />;
+  }
+
+  return <AccountCircleIcon style={{ fontSize: '35px' }} />;
 }
--- client/src/Components/Signup.jsx
+++ client/src/Components/Signup.jsx
@@ -1,10 +1,14 @@
 import { Button, TextField } from '@mui/material';
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Signup() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const navigate = useNavigate();
 
   const handleSignup = () => {
     fetch('http://localhost:3001/profile/signup/', {
@@ -14,25 +18,73 @@ export default function Signup() {
       body: JSON.stringify({
         username: username,
         password: password,
+        name: name,
+        email: email,
       }),
+    }).then(() => {
+      navigate('/');
     });
   };
 
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}>
+      <img style={{ height: '70px', marginTop: '20vh' }} src='white logo.svg' />
+      <div style={{ margin: '30px', display: 'flex' }}>
+        <div style={{ color: '#7993A1', marginRight: '5px' }}>to check by</div>{' '}
+        <div style={{ color: 'white' }}>checking</div>
+      </div>
+
       <TextField
+        style={{ marginBottom: '20px' }}
+        label='name'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder='name'
+      />
+      <TextField
+        style={{ marginBottom: '20px' }}
+        label='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder='email'
+      />
+      <TextField
+        style={{ marginBottom: '20px' }}
+        label='username'
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         placeholder='username'
       />
       <TextField
+        label='password'
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         placeholder='password'
       />
 
-      <Button onClick={handleSignup}>Sign Up</Button>
-      <Link to='/login'>Log in</Link>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Button
+          style={{ marginTop: '10px', backgroundColor: '#89A7B1' }}
+          variant='contained'
+          onClick={handleSignup}>
+          Sign up
+        </Button>
+      </div>
+
+      <div
+        style={{
+          position: 'fixed',
+          display: 'flex',
+          bottom: '20px',
+          justifyContent: 'center',
+          width: '100vw',
+        }}>
+        <Link to='/signup'>Already have an account? Log in</Link>
+      </div>
     </div>
   );
 }
