c91c9d729ca046cd9aeb2768fd8e6a2b0d479542
Eric Tao <erictao04@gmail.com>
2023-01-22 02:18:21 -0800

    fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,8 @@
 import React, { useContext } from 'react';
-import { Tab, Tabs, TabPanel } from '@mui/material';
-import GroupIcon from '@mui/icons-material/Group';
-
-import Feed from './Components/Feed/Feed';
-import ProfilePicture from './Components/Feed/ProfilePicture';
-import Todo from './Components/Todo/Todo';
 import Login from './Components/Login';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signup from './Components/Signup';
+import Main from './Components/Main';
 
 const theme = {
   primary: '34344E',
@@ -21,60 +16,15 @@ export const ThemeContext = React.createContext(theme);
 
 export default function App() {
   const theme = useContext(ThemeContext);
-  const [value, setValue] = React.useState(0);
-
-  const profileUrl =
-    'https://lh3.googleusercontent.com/ogw/AAEL6sggMroyh3_FGElpcVH9_m54NQmXHQuHLh1J_INs8g=s32-c-mo';
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
 
   return (
     <ThemeContext.Provider value={theme}>
       <BrowserRouter>
-        <Switch>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/singup'>
-            <Signup />
-          </Route>
-          <Route path='/'>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                marginBottom: '20px',
-                alignItems: 'center',
-              }}>
-              <div style={{ flexGrow: 1, cursor: 'pointer' }}>
-                <GroupIcon style={{ fontSize: '40px' }} />
-              </div>
-              <Tabs
-                value={value}
-                style={{ color: '#34344E' }}
-                TabIndicatorProps={{
-                  style: { display: 'none' },
-                }}
-                onChange={handleChange}>
-                <Tab label='Feed' value={0} />
-                <Tab label='To-do List' value={1} />
-              </Tabs>
-              <div
-                style={{
-                  flexGrow: 1,
-                  display: 'flex',
-                  justifyContent: 'end',
-                  alignItems: 'center',
-                }}>
-                <ProfilePicture url={profileUrl} />
-              </div>
-            </div>
-            {value === 0 && <Feed />}
-            {value === 1 && <Todo />}
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/' element={<Main />} />
+        </Routes>
       </BrowserRouter>
     </ThemeContext.Provider>
   );
