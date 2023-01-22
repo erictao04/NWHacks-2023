225cc086e750f34a030a8050bc070c76d24d00c6
Eric Tao <erictao04@gmail.com>
2023-01-22 01:23:11 -0800

    partialy connect tasks page

--- client/src/Components/Todo/Task.jsx
+++ client/src/Components/Todo/Task.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import CancelIcon from '@mui/icons-material/Cancel';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
-export default function Task() {
-  const taskName = 'Go to the gym';
+export default function Task({ task }) {
+  const taskName = task.task_name;
+  const completed = !!task.completed;
+
+  const handleCheck = () => {
+    fetch('http://localhost:3001/tasks/', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        task_id: task.id,
+        name: taskName,
+        completed: !completed,
+      }),
+    });
+  };
+
+  const button = completed ? (
+    <CheckCircleIcon
+      style={{ fontSize: '35px', marginLeft: '5px', color: '#CBDAD5', cursor: 'pointer' }}
+      onClick={handleCheck}
+    />
+  ) : (
+    <RadioButtonUncheckedIcon
+      style={{ fontSize: '35px', marginLeft: '5px', cursor: 'pointer' }}
+      onClick={handleCheck}
+    />
+  );
 
   return (
     <div style={{ backgroundColor: 'white', borderRadius: '30px' }}>
       <div style={{ display: 'flex', fontSize: '16px', alignItems: 'center' }}>
-        <RadioButtonUncheckedIcon style={{ fontSize: '35px', marginLeft: '5px' }} />
+        {button}
         <div
           style={{
             flexGrow: 1,
@@ -16,10 +42,18 @@ export default function Task() {
             justifyContent: 'center',
             alignItems: 'center',
             margin: '12px',
+            color: completed ? '#CBDAD5' : 'inherit',
+            textDecoration: completed ? 'line-through' : 'none',
           }}>
           {taskName}
         </div>
-        <CancelIcon style={{ fontSize: '15px', marginRight: '15px' }} />
+        <CancelIcon
+          style={{
+            fontSize: '15px',
+            marginRight: '15px',
+            visibility: completed ? 'hidden' : 'visible',
+          }}
+        />
       </div>
     </div>
   );
